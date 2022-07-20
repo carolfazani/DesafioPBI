dc5f63a48783c203814a73f34b886a6c574c2b1d
carolfazani <82125586+carolfazani@users.noreply.github.com>
2022-07-20 11:01:52 -0300

    correção de identação

--- upcsv.sql
+++ upcsv.sql
@@ -1,9 +1,8 @@
- SHOW GLOBAL VARIABLES LIKE 'local_infile';
- SET GLOBAL local_infile = true; -- alterei nas configurações também
+SHOW GLOBAL VARIABLES LIKE 'local_infile';
 SET GLOBAL local_infile = true;
- use eleicoes;
- load data local infile 'C:/Users/conta/PycharmProjects/eleicoesPBI/resultados_candidato_municipio_zona.csv' into table resultados_candidato
- fields terminated by ','
- enclosed by '"'
- lines terminated by '\n'
- (id, ano, turno, tipo_eleicao, sigla_uf,	id_municipio, id_municipio_tse,	zona, cargo, sigla_partido,	numero_candidato, sequencial_candidato,	id_candidato_bd, resultado, votos)
+use eleicoes;
+load data local infile 'resultados_candidato_municipio_zona.csv' into table resultados_candidato
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+(id, ano, turno, tipo_eleicao, sigla_uf,	id_municipio, id_municipio_tse,	zona, cargo, sigla_partido,	numero_candidato, sequencial_candidato,	id_candidato_bd, resultado, votos)
